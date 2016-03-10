7ef275642b4707d8acc53ff85d1b7e0ffcbe3b82
EsletzbichlerE <EsletzbichlerE@C302-10_8.wifi-it.at>
2016-03-10 18:25:15 +0100

    init

---
+++ application/controllers/GuestbookController.php
@@ -0,0 +1,12 @@
+<?php
+// application/controllers/GuestbookController.php
+ 
+class GuestbookController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        $guestbook = new Application_Model_GuestbookMapper();
+        $this->view->entries = $guestbook->fetchAll();
+    }
+}
+
